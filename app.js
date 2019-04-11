ec31a26519149ebfef7a9db2f45b9586b60fe30b
geraldo-cisneros <41762712+geraldo-cisneros@users.noreply.github.com>
2019-04-11 14:59:25 -0500

    prep for heroku

--- app.js
+++ app.js
@@ -1,93 +1,99 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var app = express();
-var interval;
-var interval_switch;
-var urlencodeParser = bodyParser.urlencoded({extended: false});
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var app = express()
+var interval
+var interval_switch
+var urlencodeParser = bodyParser.urlencoded({extended: false})
 
 //Import 
-Simulation = require('./models/simulate');
-SuitSwitch = require('./models/suitswitch');
+Simulation = require('./models/simulate')
+SuitSwitch = require('./models/suitswitch')
 
 //Database connector
-mongoose.connect('mongodb://localhost/spacesuit');
+mongoose.connect('mongodb://localhost/spacesuit')
 
 //EJS framework for website display
-app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets'));
+app.set('view engine', 'ejs')
+app.use('/assets', express.static('assets'))
 
 //ROUTES
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname))
 app.get('/',function(req, res){
-    res.render('index');
-});
+	res.render('index')
+})
 
 //On start button, simulation starts
 app.post('/', urlencodeParser, function(req, res){
-    console.log("--------------Simulation started--------------")
-    var time = Date.now(); 
-    interval = setInterval(Simulation.suitTelemetry.bind(null, time),1000);
-    interval_switch = setInterval(SuitSwitch.SuitSwitch,1000);
-    res.render('contact',{qs: ""});
-});
+	console.log('--------------Simulation started--------------')
+	var time = Date.now() 
+	interval = setInterval(Simulation.suitTelemetry.bind(null, time),1000)
+	interval_switch = setInterval(SuitSwitch.SuitSwitch,1000)
+	res.render('contact',{qs: ''})
+})
 
 //Returns all simulated data from the database
 app.get('/api/suit', function(req, res){      
-        Simulation.getSuitTelemetry(function (err, data) {
-            if (err) {
-                throw err;
-                console.log(err);
-            }
-            res.json(data);
-        });
-});
+	Simulation.getSuitTelemetry(function (err, data) {
+		if (err) {
+			throw err
+			console.log(err)
+		}
+		res.json(data)
+	})
+})
 
 app.get('/api/suit/recent', function(req, res){      
-    Simulation.getSuitTelemetryByDate(function (err, data) {
-        if (err) {
-            throw err;
-            console.log(err);
-        }
-        res.json(data);
-    });
-});
+	Simulation.getSuitTelemetryByDate(function (err, data) {
+		if (err) {
+			throw err
+			console.log(err)
+		}
+		res.json(data)
+	})
+})
 
 app.get('/api/suitswitch', function(req, res){      
-    SuitSwitch.getSuitSwitch(function (err, data) {
-        if (err) {
-            throw err;
-            console.log(err);
-        }
-        res.json(data);
-    });
-});
+	SuitSwitch.getSuitSwitch(function (err, data) {
+		if (err) {
+			throw err
+			console.log(err)
+		}
+		res.json(data)
+	})
+})
 
 app.get('/api/suitswitch/recent', function(req, res){      
-    SuitSwitch.getSuitSwitchByDate(function (err, data) {
-        if (err) {
-            throw err;
-            console.log(err);
-        }
-        res.json(data);
-    });
-});
+	SuitSwitch.getSuitSwitchByDate(function (err, data) {
+		if (err) {
+			throw err
+			console.log(err)
+		}
+		res.json(data)
+	})
+})
 
 app.get('/contact',function(req, res){
-    res.render('contact',{qs: req.query});
-});
+	res.render('contact',{qs: req.query})
+})
 
 app.post('/contact', urlencodeParser, function(req, res){
-    //console.log(req.body);
-    console.log('--------------Simulation stopped--------------');
-    clearInterval(interval);
-    clearInterval(interval_switch);
-    res.render('contact-success',{data: req.body});
-});
+	//console.log(req.body);
+	console.log('--------------Simulation stopped--------------')
+	clearInterval(interval)
+	clearInterval(interval_switch)
+	res.render('contact-success',{data: req.body})
+})
 
 /* app.get('/profile/:name', function(req, res){
     var data = {age:29, job: 'Astronaut', hobbies: ['eating', 'fighting', 'fishing']};
     res.render('profile', {person: req.params.name, data:data});
 }) */
 
-app.listen(3000);
-console.log('Server is running on port 3000...');
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'))
+})
+//app.listen(3000)
+console.log('Server is running on port 3000...')
